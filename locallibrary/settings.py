ffbe2ce7b92655483bba12872dc9faeab44d2ce9
yarelbv <yarel21@outlook.es>
2022-03-28 22:39:40 -0600

    host

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['young-refuge-23895.herokuapp.com','127.0.0.1']
 
 
 # Application definition
